b372753c4336b5807859c19d303f281416353d9f
Lede-dev <lede.7716@gmail.com>
2025-02-18 06:00:41 +0900

    fix: fix typo

--- src/main/java/com/jamplestudio/coj/protocol/data/ChatSettingsResponse.java
+++ src/main/java/com/jamplestudio/coj/protocol/data/ChatSettingsResponse.java
@@ -2,7 +2,7 @@ package com.jamplestudio.coj.protocol.data;
 
 import org.jetbrains.annotations.NotNull;
 
-public record ChatSettingsRequest(
+public record ChatSettingsResponse(
         @NotNull String chatAvailableCondition,
         @NotNull String chatAvailableGroup,
         int minFollowerMinute,
