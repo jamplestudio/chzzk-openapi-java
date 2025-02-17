b527931ebdc2771fc3f7197aee2deeca25b131ba
Lede-dev <lede.7716@gmail.com>
2025-02-18 05:54:36 +0900

    feat: added http client to parameter

--- src/main/java/com/jamplestudio/coj/protocol/http/executors/HttpRequestExecutor.java
+++ src/main/java/com/jamplestudio/coj/protocol/http/executors/HttpRequestExecutor.java
@@ -1,11 +1,12 @@
 package com.jamplestudio.coj.protocol.http.executors;
 
+import okhttp3.OkHttpClient;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Optional;
 
 public interface HttpRequestExecutor<Request, Response> {
 
-    @NotNull Optional<Response> execute(@NotNull Request request);
+    @NotNull Optional<Response> execute(@NotNull OkHttpClient client, @NotNull Request request);
 
 }
